1cb0feaa8b37445f5a49254023d70a06f71dfd35
Anthony Kühl <143295353+Mediateam-Ros@users.noreply.github.com>
2025-07-15 12:41:28 +0200

    credentials

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,8 +1,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
 // 🔐 Deine Supabase-Daten:
-const supabaseUrl = 'https://uedocrmntzzxescgmpkw.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlZG9jcm1udHp6eGVzY2dtcGt3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc1NTc0NTUsImV4cCI6MjA2MzEzMzQ1NX0.6C6OmnYBouLHTtlOAVoq4JQM-5o9j9YvIadYoKP5DC0'
+const supabaseUrl = 'https://hictaaxxrrddgbkzyvnp.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpY3RhYXh4cnJkZGdia3p5dm5wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI1NzU0MjEsImV4cCI6MjA2ODE1MTQyMX0.kr3DRMgCqM977ANdjmrJkCJ-FmXbWubk1ikxXk28DLg'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 const form = document.getElementById('login-form')
